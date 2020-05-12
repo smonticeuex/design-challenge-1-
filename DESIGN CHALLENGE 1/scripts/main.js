cc5170322cb95eb29b5c4e078c1870ccd33f28c0
smonticeuex <58290699+smonticeuex@users.noreply.github.com>
2020-05-12 09:58:59 -0400

    Javascript added

--- DESIGN CHALLENGE 1/scripts/main.js
+++ DESIGN CHALLENGE 1/scripts/main.js
@@ -1,1 +1,36 @@
+//Step 1: Add Random Input
+
+var fuelStart = 1000000; 
+var distanceStart = 0;
+var oxygenStart = 100;
+
+var currentFuel = ((fuelStart -10)*1.5);
+var currentDistance = ((distanceStart +2)*100);
+var currentOxygen= ((oxygenStart -1)*0.9);
+
+//Step 2: Add 3 functions
+//Step 3: Make it visible
+
+function newFuel (){ 
+    var elem = document.getElementById("fuel");
+	elem.innerHTML = Math.floor(currentFuel);
+}
+
+newFuel();
+
+function newDistance (){ 
+    var elem = document.getElementById("distance");
+	elem.innerHTML = Math.floor(currentDistance);
+}
+
+newDistance();
+
+function newOxygen (){ 
+    var elem = document.getElementById("oxygen");
+	elem.innerHTML = Math.floor(currentOxygen);
+}
+
+newOxygen();
+
+console.log(newFuel, newDistance, newOxygen);
 
