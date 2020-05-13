7d72f666f142573693ef6558cfef1c701e684b22
smonticeuex <58290699+smonticeuex@users.noreply.github.com>
2020-05-13 03:48:17 -0400

    complete! :)

--- DESIGN CHALLENGE 1/scripts/main.js
+++ DESIGN CHALLENGE 1/scripts/main.js
@@ -1,16 +1,19 @@
-//Step 1: Add Random Input
+//Creative assignment #1 
 
+//Step 1: Add random rocket data:
 var fuelStart = 1000000; 
 var distanceStart = 0;
 var oxygenStart = 100;
 
+
+//Step 2: Add random formulas to determine rockets current data:
 var currentFuel = ((fuelStart -10)*1.5);
 var currentDistance = ((distanceStart +2)*100);
 var currentOxygen= ((oxygenStart -1)*0.9);
 
-//Step 2: Add 3 functions
-//Step 3: Make it visible
 
+//Step 3: Add 3 functionalities 
+//Step 4: Make results visible on the website
 function newFuel (){ 
     var elem = document.getElementById("fuel");
 	elem.innerHTML = Math.floor(currentFuel);
